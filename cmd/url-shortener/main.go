e483d2abcb2ce73ffa68f3a28c6d75aaa8dd30b7
Детер Климентий <kkdeter@edu.hse.ru>
2025-08-21 16:46:31 +0300

    Implement logger

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -2,16 +2,24 @@ package main
 
 import (
 	"URL-shortener/internal/config"
-	"fmt"
+	"log/slog"
 	_ "log/slog"
+	"os"
+)
+
+const (
+	envLocal = "local"
+	envProd  = "prod"
+	envDev   = "dev"
 )
 
 func main() {
 	// TODO: init config: cleanenv
 	cfg := config.MustLoad()
 
-	fmt.Printf("%+v\n\n", cfg)
+	log := setupLogger(cfg.Env)
 
+	log.Info("starting server")
 	//TODO init logger: slog
 
 	//TODO: init storage: postgress
@@ -19,4 +27,27 @@ func main() {
 	//TODO: init router: chi, chi-render
 
 	//TODO: run server
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout,
+				&slog.HandlerOptions{Level: slog.LevelDebug},
+			),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout,
+				&slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout,
+				&slog.HandlerOptions{Level: slog.LevelInfo}))
+	}
+
+	return log
 }
